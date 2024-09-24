38a27b8a26e8392e1210f719e216d018825cd2d1
Gowri Mohan <gowrimohan@Gowris-MacBook-Pro.local>
2024-09-24 10:43:14 +0530

    added contact info and deleted headers

--- obj/Debug/net8.0/portfolio.AssemblyInfo.cs
+++ obj/Debug/net8.0/portfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e36cc69095dc26d177324aa8e0e74b029e7aa30")]
 [assembly: System.Reflection.AssemblyProductAttribute("portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
