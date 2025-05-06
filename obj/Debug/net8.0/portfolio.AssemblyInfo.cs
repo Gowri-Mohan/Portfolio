38a7a06564e4b2c8f3851bfcf3b99c1e106bc902
Gowri Mohan <gowrimohan@Gowris-MacBook-Pro.local>
2025-05-06 19:30:02 +0530

    almost there

--- obj/Debug/net8.0/portfolio.AssemblyInfo.cs
+++ obj/Debug/net8.0/portfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82cea27393ace2070a005c610f8fc55cd8045f75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dcfef41a478a7377149654e66c1bfa7ec098a50")]
 [assembly: System.Reflection.AssemblyProductAttribute("portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
